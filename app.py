dc50bd59ddfa848994436454e2a584d25e2a4b93
deepank chauhan <deepankchauhan99@gmail.com>
2024-02-16 07:27:40 +0530

    add conf changes

--- app.py
+++ app.py
@@ -8,12 +8,20 @@ def parse(file):
         data = json.load(json_file)
         return data['sites']
 
+config = 'conf.json'
+site_details = parse(config)[0]
+
+# fetch butler ip from the conf.json
+host = next((server['ip'] for server in site_details['ips'] if server['server'] == 'butler'), None)
+print(host)
+
 @app.route('/')
 def index():
-    config = 'conf.json'
-    sitename = parse(config)[0]
+    sitename = site_details['name']
     print(sitename)
+    
     return render_template('index.html', sitename=sitename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=host, port=5005)
+    # app.run(debug=True)
